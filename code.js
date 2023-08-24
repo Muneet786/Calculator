fdf217922f49af9a053c550d54e0fbf83cb1dba1
Muneet786 <kumarmuneet440@gmail.com>
2023-08-24 10:30:55 -0700

    work done

---
+++ code.js
@@ -0,0 +1,17 @@
+function dabaya(e){
+    document.getElementById('inp').value += e
+}
+
+function eq(){
+    var a = document.getElementById('inp')
+    a.value = eval(a.value)
+}
+
+function clr(){
+    document.getElementById('inp').value = " "
+}
+
+function ccc(){
+    var a = document.getElementById('inp')
+    a.value = a.value.slice(0,-1)
+}
